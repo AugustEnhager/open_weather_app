bac4004c48cb431f815ffdfab98058d65306bfe9
August <august.enhager@gmail.com>
2021-09-14 17:23:25 +0200

    adding position coordinates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [geolocation, setGeolocation] = useState({});
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    setGeolocation(position.coords);
+    debugger;
+  });
+
   return (
     <div>
       <h1>Hello world</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
