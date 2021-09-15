ddf0ee34526b5acb4b463ef27f0148bed9e5d1a0
August <august.enhager@gmail.com>
2021-09-15 15:28:19 +0200

    updates app.jsx to get location- and weatherResponse values

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import axios from 'axios'
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export class App extends Component {
   state = {
@@ -8,10 +9,9 @@ export class App extends Component {
   
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(async position => {
-      
       let { latitude, longitude } = position.coords
       let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=62ec85f74ac844b6a9b671b840175735`)
-      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=c1d9dbe5c0dfaac680ddbc9e7a19e3341`)
+      let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=c1d9dbe5c0dfaac680ddbc9e7a19e334`)
       debugger
     })
   }
