f686c1fafb1d7daa7412196c3d56fc7101fa82e9
serge_kamya <sergekamya@gmail.com>
2021-09-15 14:31:27 +0200

    bughunting App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ export class App extends Component {
   
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(async position => {
+      
       let { latitude, longitude } = position.coords
-      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=a6011dafb92e4c91824ac448c7973c0c`)
+      let locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=62ec85f74ac844b6a9b671b840175735`)
       let weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=c1d9dbe5c0dfaac680ddbc9e7a19e3341`)
       debugger
     })
