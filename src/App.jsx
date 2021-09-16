fa5db0283afc121517d2f5db07c3b6bec4eb70e4
serge_kamya <sergekamya@gmail.com>
2021-09-16 18:50:23 +0200

    added semantic design in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Item } from "semantic-ui-react";
+import { Item, Header, List } from "semantic-ui-react";
 import { Line } from "react-chartjs-2";
 
 export class App extends Component {
@@ -40,27 +40,27 @@ export class App extends Component {
         weather: weatherResponse.data.current.weather[0].main,
       };
 
-      debugger
-     
+      debugger;
+
       this.setState({ location: weatherInfo });
     });
   }
 
   render() {
-    const { location, dailyTemp} = this.state
-    let labels = []
-    let dataItems = []
-    let data
-    if(dailyTemp) {
-      dailyTemp.forEach(day => {       
-        labels.push(new Date(day.dt * 1000).toLocaleDateString())
-        dataItems.push(day.temp.day)
-      })
-      data = { labels: labels, datasets: [{ label: "Daily Temp", data: dataItems}]}
+    const { location, dailyTemp } = this.state;
+    let labels = [];
+    let dataItems = [];
+    let data;
+    if (dailyTemp) {
+      dailyTemp.forEach((day) => {
+        labels.push(new Date(day.dt * 1000).toLocaleDateString());
+        dataItems.push(day.temp.day);
+      });
+      data = {
+        labels: labels,
+        datasets: [{ label: "Daily Temp", data: dataItems }],
+      };
     }
-
-
-
 
     const temp = this.state.location.temp;
     const city = this.state.location.city;
@@ -70,14 +70,41 @@ export class App extends Component {
 
     return (
       <div>
-        <h1 data-testid="header">Welcome to Weather App 3000</h1>
+        <Header data-testid="header">Welcome to Weather App 3000</Header>
         <div data-cy="weather-display">
-          <div data-cy="location">Location: {city} </div>
-          <div data-cy="temp">Temperature: {temp}°C</div>
-          <div data-cy="humidity">Humidity: {humidity}%</div>
-          <div data-cy="windspeed">Windspeed: {windspeed}m/s </div>
-          <div data-cy="weather"> Weather: {weather}</div>
-          <Line data={data}/>
+          <div class="ui relaxed divided list">
+            <div class="item">
+              <i class="large world middle aligned icon"></i>
+              <div class="content">
+                <a class="header">Location</a>
+                <div class="description">{city}</div>
+              </div>
+            </div>
+            <div class="item">
+              <i class="large github middle aligned icon"></i>
+              <div class="content">
+                <a class="header">Semantic-Org/Semantic-UI-Docs</a>
+                <div class="description">Updated 22 mins ago</div>
+              </div>
+            </div>
+            <div class="item">
+              <i class="large github middle aligned icon"></i>
+              <div class="content">
+                <a class="header">Semantic-Org/Semantic-UI-Meteor</a>
+                <div class="description">Updated 34 mins ago</div>
+              </div>
+            </div>
+          </div>
+
+          <List data-cy="location">Location: {city} </List>
+          <List data-cy="temp">Temperature: {temp}°C</List>
+          <List data-cy="humidity">Humidity: {humidity}%</List>
+          <List data-cy="windspeed">Windspeed: {windspeed}</List>
+          <List data-cy="weather">Weather: {weather} </List>
+
+          {/* </div><div class="ui list"> */}
+
+          <Line data={data} />
         </div>
       </div>
     );
